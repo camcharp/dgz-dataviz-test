30f3a9d11525335b39bb142105f7cc75b35eb47b
Camille Charpentier <cam.charpentier@live.fr>
2019-09-02 16:05:50 +0200

    created first version of chart

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import Grid from './Grid';
+import Grid from './Grid';
 import trajectoires from '../trajectoires.json';
 
 let sortedTrajectories = [];
@@ -8,27 +8,34 @@ export default class Board extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			trajectoires: trajectoires,
-			client: null,
+			trajectoires: [],
+			client: null
 		};
-    }
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-    componentWillMount() {
-        this.sortTrajectoriesByTime()
-        this.setState({trajectoires : sortedTrajectories})
-    }
-    
 	sortTrajectoriesByTime = () => {
 		trajectoires.forEach((client) => {
 			client.points.sort((a, b) => {
 				return a.time - b.time;
-            });
-            sortedTrajectories.push(client);
-        });
+			});
+			sortedTrajectories.push(client);
+		});
 	};
 
+	componentWillMount() {
+		this.sortTrajectoriesByTime();
+		this.setState({ trajectoires: sortedTrajectories });
+	}
+
+	handleChange(event) {
+		this.setState({
+			client: this.state.trajectoires.find((client) => client.id === event.target.value)
+		});
+	}
+
 	render() {
-        console.log( this.state.trajectoires)
+		console.log(this.state.trajectoires);
 		const clients = this.state.trajectoires.map((client, index) => {
 			return (
 				<option name="client" key={index} value={client.id} onChange={this.handleChange}>
@@ -43,6 +50,7 @@ export default class Board extends Component {
 				<select id="select-client" onChange={this.handleChange}>
 					{clients}}
 				</select>
+				<Grid data={this.state.client} />
 			</React.Fragment>
 		);
 	}
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,11 +1,45 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
-export default class Grid extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+export default class Grid extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			client: {
+				id: null,
+				points: null
+			},
+			data: []
+		};
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.setState({ client: nextProps.data });
+	}
+
+	componentDidUpdate() {
+		this.setState({ data: this.state.client.points });
+	}
+
+	render() {
+		console.log(this.props);
+		return (
+			<ScatterChart
+				width={500}
+				height={400}
+				margin={{
+					top: 20,
+					right: 20,
+					bottom: 20,
+					left: 20
+				}}
+			>
+				<CartesianGrid />
+				<XAxis  type="number" dataKey="x" name="x" />
+				<YAxis  yAxisId="left" type="number" dataKey="y" name="y" stroke="#8884d8" />
+				<Tooltip cursor={{ strokeDasharray: '3 3' }} />
+				<Scatter yAxisId="left" name="A school" data={this.state.data} fill="#8884d8" />
+			</ScatterChart>
+		);
+	}
 }
