cbf2a23d353efea562b9752048d0b7fe0338a0f7
Camille Charpentier <cam.charpentier@live.fr>
2019-09-02 15:54:36 +0200

    sorted points by time

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,13 +1,49 @@
-import React, { Component } from 'react'
-import Grid from './Grid';
+import React, { Component } from 'react';
+// import Grid from './Grid';
 import trajectoires from '../trajectoires.json';
 
+let sortedTrajectories = [];
+
 export default class Board extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+	constructor(props) {
+		super(props);
+		this.state = {
+			trajectoires: trajectoires,
+			client: null,
+		};
     }
+
+    componentWillMount() {
+        this.sortTrajectoriesByTime()
+        this.setState({trajectoires : sortedTrajectories})
+    }
+    
+	sortTrajectoriesByTime = () => {
+		trajectoires.forEach((client) => {
+			client.points.sort((a, b) => {
+				return a.time - b.time;
+            });
+            sortedTrajectories.push(client);
+        });
+	};
+
+	render() {
+        console.log( this.state.trajectoires)
+		const clients = this.state.trajectoires.map((client, index) => {
+			return (
+				<option name="client" key={index} value={client.id} onChange={this.handleChange}>
+					{client.id}
+				</option>
+			);
+		});
+		return (
+			<React.Fragment>
+				<h1>Visualiser les clients</h1>
+				<label htmlFor="select-client">Choisissez un client par ID:</label>
+				<select id="select-client" onChange={this.handleChange}>
+					{clients}}
+				</select>
+			</React.Fragment>
+		);
+	}
 }
