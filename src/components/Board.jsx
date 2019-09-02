f72ae0e46938f997d77e3ac466ccc6e104c215bc
Camille Charpentier <cam.charpentier@live.fr>
2019-09-02 14:45:44 +0200

    created components + imported json file

---
+++ src/components/Board.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Grid from './Grid';
+import trajectoires from '../trajectoires.json';
+
+export default class Board extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
